5c37c1aa4b79ea0c9e7acfb08c65d8a4d3b8cc16
Mohamed Amine Nafia <m.amine.nafia@gmail.com>
2025-08-31 21:34:19 +0200

    fix the code

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -4,7 +4,6 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <Text>Entry Screen - Awesome</Text>
-
       <Text style={styles.largeText}>
         Typescript is great if you practice more
       </Text>
