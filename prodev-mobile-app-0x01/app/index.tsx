99738edd4f6576df8dd53738d9387670ca55ed35
Mohamed Amine Nafia <m.amine.nafia@gmail.com>
2025-09-01 01:02:24 +0200

    fix the code

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -2,14 +2,14 @@ import * as React from "react";
 import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
-  return (
-    <View style={styles.container}>
-      <Text>Entry Screen - Awesome</Text>
-      <View>
-        <Text style={styles.largeText}>Typescript is great if you practice more</Text>\r\n
-        <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>\r\n
-        <Text style={styles.smallText}>ALX is awesome</Text>
-      </View>
+return (
+  <View style={styles.container}>
+  <Text>Entry Screen - Awesome</Text>
+  <View>
+  <Text style={styles.largeText}>Typescript is great if you practice more</Text>
+  <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
+  <Text style={styles.smallText}>ALX is awesome</Text>
+  </View>
     </View>
   );
 }
